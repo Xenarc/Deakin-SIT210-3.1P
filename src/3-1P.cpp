2139c47db1567c6994bd0c147ce6e80d90e38371
Mark Blashki <markblashki1@gmail.com>
2022-04-15 17:36:18 +1000

    add: Light Meter Monitoring

--- src/3-1P.cpp
+++ src/3-1P.cpp
@@ -4,16 +4,36 @@
 
 #include "Particle.h"
 #line 1 "g:/Mark/Documents/DOCUMENTS/Deakin/SIT210_Embedded_Systems_Development/3.1P/3-1P/src/3-1P.ino"
-#include <BH1750.h>
+#include "..\lib\BH1750\src\BH1750.h"
+#include <Wire.h>
 void setup();
 void loop();
-#line 2 "g:/Mark/Documents/DOCUMENTS/Deakin/SIT210_Embedded_Systems_Development/3.1P/3-1P/src/3-1P.ino"
-BH1750 LightSensor;
+#line 3 "g:/Mark/Documents/DOCUMENTS/Deakin/SIT210_Embedded_Systems_Development/3.1P/3-1P/src/3-1P.ino"
+BH1750 lightMeter;
 
-void setup() {
+// Wait for Network to become live before sending
+// SYSTEM_THREAD(ENABLED);
 
+void setup(){
+
+  Serial.begin(9600);
+  Serial.println(F("Ready"));
+
+  Wire.begin();
+
+  lightMeter.begin();
+  lightMeter.switch_power_on();
+  Serial.println(F("BH1750 Setup"));
 }
 
 void loop() {
-  
+
+  float lux = lightMeter.get_light_level();
+  Serial.print("Light: ");
+  Serial.print(std::to_string(lux).c_str());
+  Serial.print(" lx\t");
+  bool status = Particle.publish("MBlashkiArgon_LightMeter", std::to_string(lux).c_str(), PRIVATE);
+  Serial.print("Status: ");
+  Serial.println(status);
+  delay(30000);
 }
--- src/3-1P.ino
+++ src/3-1P.ino
@@ -1,10 +1,30 @@
-#include <BH1750.h>
-BH1750 LightSensor;
+#include "..\lib\BH1750\src\BH1750.h"
+#include <Wire.h>
+BH1750 lightMeter;
 
-void setup() {
+// Wait for Network to become live before sending
+// SYSTEM_THREAD(ENABLED);
 
+void setup(){
+
+  Serial.begin(9600);
+  Serial.println(F("Ready"));
+
+  Wire.begin();
+
+  lightMeter.begin();
+  lightMeter.switch_power_on();
+  Serial.println(F("BH1750 Setup"));
 }
 
 void loop() {
-  
+
+  float lux = lightMeter.get_light_level();
+  Serial.print("Light: ");
+  Serial.print(std::to_string(lux).c_str());
+  Serial.print(" lx\t");
+  bool status = Particle.publish("MBlashkiArgon_LightMeter", std::to_string(lux).c_str(), PRIVATE);
+  Serial.print("Status: ");
+  Serial.println(status);
+  delay(30000);
 }
